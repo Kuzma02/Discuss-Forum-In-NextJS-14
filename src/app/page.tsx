73262959aa1fbd3bdc9f2c5ec2c8877df48d9b69
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2024-01-12 18:56:03 +0100

    auth_added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@nextui-org/react";
+
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me!!!</Button>
-    </div>
-  )
+  return <div>Home Page</div>
 }
