fbd8e45e0b7966a28c28c3653b72a78917570b07
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2024-01-13 09:31:04 +0100

    finishing_form_validation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,7 +1,42 @@
 "use server";
 
-export async function createTopic() {
+import { z } from "zod";
 
-    // TODO: revalidate the homepage 
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be a lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+) : Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+        errors: result.error.flatten().fieldErrors
+    }
+  }
+
+  return {
+    errors: {}
+  };
+
+  // TODO: revalidate the homepage
 }
